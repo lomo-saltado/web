2a217e027381f91129a8de80811617068d15b32b
David S <96256938+lomo-saltado@users.noreply.github.com>
2023-02-15 01:43:08 -0500

    Add files via upload

--- scholl/java/script2.js
+++ scholl/java/script2.js
@@ -18,7 +18,7 @@ popupWhatsApp = () => {
   let msg = document.getElementById('whats-in').value;
   let relmsg = msg.replace(/ /g,"%20");
      
-   window.open('https://wa.me/51987654321?text='+relmsg, '_blank'); 
+   window.open('https://wa.me/51999583448?text='+relmsg, '_blank'); 
   
   });
 
